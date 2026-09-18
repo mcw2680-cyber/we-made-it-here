67087b50e86133e2c14ba27185d7286f93c019ba
mcw2680-cyber <mcw2680@gmail.com>
2026-09-17 18:33:20 -0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,7 +1,11 @@
+
 // Shopify connection settings.
+// Leave blank to use the built-in demo catalog.
 // IMPORTANT: Only use the PUBLIC Storefront API token here.
+// Never put a private Admin API token or private Storefront token in this file.
+
 window.SAMIH_CONFIG = {
-  shopDomain: "jy98yf-de.myshopify.com",
-  publicStorefrontToken: "9c50f2ae6cf7b3f92530ea979d9c9625",
+ shopDomain: "jy98yf-de.myshopify.com",
+publicStorefrontToken: "9c50f2ae6cf7b3f92530ea979d9c9625",
   apiVersion: "2026-07"
 };
